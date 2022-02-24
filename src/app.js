88629e5d8cb33e1eac2cf7cb27df880feb9f8a36
YuriiMakogonov <yurii.a.makogonov@gmail.com>
2022-02-24 14:43:03 +0300

    update app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
+const port = process.env.PORT || 8080;
 
 const app = express();
 // определяем обработчик для маршрута "/"
-app.get("/", function(request, response) {
+app.get("/", function (request, response) {
 
+    console.log('request');
     // send response
     response.send("Generate Signature APP");
 });
 // start listening port 3000
-// app.listen(3000);
+app.listen(port);
