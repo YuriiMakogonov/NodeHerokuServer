2e9369afb1b989d1620676fe2aa9bcf2ae9ad7e4
YuriiMakogonov <yurii.a.makogonov@gmail.com>
2022-03-01 16:38:04 +0300

    add crypto-js framework. get signature

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const CryptoJS = require("crypto-js");
 const port = process.env.PORT || 8080;
 
 const app = express();
-// определяем обработчик для маршрута "/"
+
 app.get("/", function (request, response) {
 
     console.log('request');
-    // send response
-    response.send("Generate Signature APP");
+
+    let test = 'secret';
+    let testCrypto = CryptoJS.PBKDF2(test, 'salt', {keySize: 256/32, iterations: 2048, hasher: CryptoJS.algo.SHA256});
+    console.log('testCrypto', testCrypto);
+
+    let signature = CryptoJS.enc.Hex.stringify(testCrypto);
+    console.log('signature', signature);
+    response.send("Generate Signature APP2 signature: " + signature);
 });
-// start listening port 3000
+
 app.listen(port);
